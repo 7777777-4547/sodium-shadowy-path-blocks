db6b8455b456242b811a1fd61e201c302fac7332
7777777_4547 <83630775+7777777-4547@users.noreply.github.com>
2022-06-18 23:06:52 +0800

    i18n

--- src/main/java/rynnavinx/sspb/client/gui/SSPBGameOptionPages.java
+++ src/main/java/rynnavinx/sspb/client/gui/SSPBGameOptionPages.java
@@ -9,6 +9,8 @@ import me.jellysquid.mods.sodium.client.gui.options.OptionPage;
 import me.jellysquid.mods.sodium.client.gui.options.control.ControlValueFormatter;
 import me.jellysquid.mods.sodium.client.gui.options.control.SliderControl;
 import me.jellysquid.mods.sodium.client.gui.options.control.TickBoxControl;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.TranslatableText;
 
 import rynnavinx.sspb.client.gui.options.storage.SSPBOptionsStorage;
 
@@ -25,21 +27,21 @@ public class SSPBGameOptionPages {
         List<OptionGroup> groups = new ArrayList<>();
         groups.add(OptionGroup.createBuilder()
                 .add(OptionImpl.createBuilder(int.class, sspbOpts)
-                        .setName("Shadowyness")
-                        .setTooltip("Adjusts the strength of the vanilla-like shadows on non-full blocks\n\n0% - None (Sodium Default)\n85% - Default\n100% - Max")
+                        .setName(new TranslatableText("sspb.options.shadowyness.name").getString())
+                        .setTooltip(new TranslatableText("sspb.options.shadowyness.tooltip").getString())
                         .setControl(option -> new SliderControl(option, 0, 100, 1, ControlValueFormatter.percentage()))
                         .setBinding(SSPBGameOptions::updateShadowyness, opts -> opts.shadowynessPercent)
                         .setFlags(OptionFlag.REQUIRES_RENDERER_RELOAD)
                         .build())
                 .add(OptionImpl.createBuilder(boolean.class, sspbOpts)
-                        .setName("Only affect path blocks")
-                        .setTooltip("The bug in vanilla Minecraft that causes shadowy path blocks actually affects all non-fluid non-full blocks\n\nBy default, SSPB applies the vanilla-like smooth lighting to all non-fluid non-full blocks as well. Enabling this will limit the effects to path blocks only.")
+                        .setName(new TranslatableText("sspb.options.onlyaffectpathblocks.name").getString())
+                        .setTooltip(new TranslatableText("sspb.options.onlyaffectpathblocks.tooltip").getString())
                         .setControl(TickBoxControl::new)
                         .setBinding((opts, value) -> opts.onlyAffectPathBlocks = value, opts -> opts.onlyAffectPathBlocks)
                         .setFlags(OptionFlag.REQUIRES_RENDERER_RELOAD)
                         .build())
                 .build());
 
-        return new OptionPage("SSPB", ImmutableList.copyOf(groups));
+        return new OptionPage(new TranslatableText("sspb.pages.sspb_page.name").getString(), ImmutableList.copyOf(groups));
     }
 }
